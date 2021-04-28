54d34da5c4c556dc9d04aa7b15aa453dd8525289
Timothy Thelen <t.thelen@criteo.com>
2021-04-28 09:36:59 -0400

    Initial Commit

---
+++ cron.api.invocation/ServiceSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace cron.api.invocation
+{
+    public class ServiceSettings
+    {
+        public string AuthUrl { get; set; }
+        public string ServiceUrl { get; set; }
+        public string Cron { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
