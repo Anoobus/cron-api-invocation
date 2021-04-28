4e12197cbb2af91f34aefab2609512fb3d38d97b
Timothy Thelen <t.thelen@criteo.com>
2021-04-28 12:11:49 -0400

    tweak intaller for orion VM

--- install-service.ps1
+++ install-service.ps1
@@ -1,26 +1,41 @@
 ﻿# Script must be run as an admin to create event viewer source
 #Requires -RunAsAdministrator
 
+Write-Host get date identifier
+$dateId=Get-Date -Format "MMM-dd-yyyy"
+$dateId="cron-svc-build-$dateId"
+
+Write-Host create sql data dir for this build
+mkdir "..\$dateId\sources"
+
 # Setup the Event Viewer source
-$logFileExists = [System.Diagnostics.EventLog]::SourceExists("Orion Service Source");
+$logFileExists = [System.Diagnostics.EventLog]::SourceExists("PSS Cron Service Source");
 if (! $logFileExists) {
 	Write-Host "Creating event source..."
-	[System.Diagnostics.EventLog]::CreateEventSource("Orion Service Source", "Application")
+	[System.Diagnostics.EventLog]::CreateEventSource("PSS Cron Service Source", "Application")
 	Write-Host "Event source created successfully"
 }
 
-# Do we need to pull down the code or will that already be done with the main app?
+
+Write-Host get latest source code
+#set tls level so we can download the zip
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "https://github.com/Anoobus/cron-api-invocation/archive/master.zip" -OutFile "..\pss-cron-master.zip"
+
+Write-Host unzip the file
+Expand-Archive -LiteralPath "..\pss-cron-master.zip" -DestinationPath "..\$dateId\sources"
+del  "..\pss-cron-master.zip"
 
 # Need to determine where it should be installed
-$destination = 'c:\orion-service'
+$destination = "c:\netcore-app\$dateId"
 
 # Setup the service info
 $params = @{
-	Name = "orion.service"
-	BinaryPathName = "$destination\orion.service.exe"
-	DisplayName = "Orion Service"
+	Name = "pss-cron-service"
+	BinaryPathName = "$destination\cron.api.invocation.exe"
+	DisplayName = "PSS Cron Service"
 	StartupType = "Auto"
-	Description = "The Orion Service"
+	Description = "The PSS Cron Service (call APIs at cron schedule intervals)"
 }
 
 # If the service already exists stop it before replacing the code
@@ -32,7 +47,7 @@ if($service -ne $null) {
 
 # Build and publish the Service
 Write-Host "Publishing service"
-dotnet publish ".\orion.service.csproj" -c Release -o $destination
+dotnet publish ".\$dateId\sources\cron.api.invocation.csproj" -c Release -o $destination
 
 # Install the Windows Service if needed
 if($service -eq $null) {
